c571bdb4fa7a5d0e9a3f9719160f3019e1249579
omartins-zs <gabrielmatheus209@gmail.com>
2025-03-09 13:53:23 -0300

    Ajuste no Hardware Product Request

--- app/Http/Requests/HardwareProductRequest.php
+++ app/Http/Requests/HardwareProductRequest.php
@@ -6,23 +6,18 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class HardwareProductRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
+    public function authorize()
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
+    public function rules()
     {
         return [
-            //
+            'name'        => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'price'       => 'required|numeric|min:1',
+            'stock'       => 'required|integer|min:0',
         ];
     }
 }
