61e85c810e4fe1a0551aa36f7918fa8acd450c88
omartins-zs <gabrielmatheus209@gmail.com>
2025-03-09 13:42:14 -0300

    Ajustando Estrutura da Tabela de Logs

--- database/migrations/2025_03_08_164809_create_logs_table.php
+++ database/migrations/2025_03_08_164809_create_logs_table.php
@@ -4,24 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateLogsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('logs', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('level');
+            $table->text('message');
+            $table->json('context')->nullable();
+            // Se você preferir usar apenas created_at, desabilite updated_at
+            $table->timestamp('created_at')->useCurrent();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('logs');
     }
-};
+}
