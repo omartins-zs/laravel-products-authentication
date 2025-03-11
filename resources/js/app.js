074ab77ec7a25480b908fa41a5cacfb00a9ad2a7
omartins-zs <gabrielmatheus209@gmail.com>
2025-03-11 00:37:16 -0300

    Add e Personalizando o Toastr

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,20 @@
-import './bootstrap';
+import toastr from 'toastr';
+import 'toastr/build/toastr.min.css';
 
-import Alpine from 'alpinejs';
-
+window.toastr = toastr;
 window.Alpine = Alpine;
 
 Alpine.start();
+
+// Personalização do Toastr
+toastr.options = {
+    "closeButton": true,
+    "progressBar": true,
+    "positionClass": "toast-top-center",
+    "timeOut": 2000,
+    "extendedTimeOut": 1000,
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+};
