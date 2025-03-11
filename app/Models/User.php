28029fd01d40f6b0773831dbfda45c215bb26322
omartins-zs <gabrielmatheus209@gmail.com>
2025-03-10 23:46:35 -0300

    Adicionando o Relacionamento com os Logs

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function logs()
+    {
+        return $this->hasMany(LogEntry::class, 'user_id');
+    }
 }
