0ab808672c4caf6cb6993c9fa8dbac51e3ea7c84
omartins-zs <gabrielmatheus209@gmail.com>
2025-03-18 17:21:03 -0300

    Alterando Max_file_size

--- config/filesystems.php
+++ config/filesystems.php
@@ -42,6 +42,7 @@ return [
             'driver' => 'local',
             'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
+            'max_file_size' => 10240, // 10MB
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
